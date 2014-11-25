c0e2e891bbc6545965c6303bfa660f12eccfc7b6
abhinavbansal246@gmail.com <abhinavbansal246@gmail.com>
2014-11-24 21:48:38 -0600

    Adding the filereader to read the all files from all subdirs

--- StringMatching/src/algorithms/IterateDir.java
+++ StringMatching/src/algorithms/IterateDir.java
@@ -5,19 +5,19 @@ import java.io.*;
 public class IterateDir 
 {
 	public static void main(String... args) {
-	    File[] files = new File("/Users/Devcenter/Dropbox/padhai/sem3/Algo").listFiles();
-	    showFiles(files);
+		File[] files = new File("/Users/Devcenter/Dropbox/padhai/sem3/Algo").listFiles();
+		showFiles(files);
 	}
 
 	public static void showFiles(File[] files) {
-	    for (File file : files) {
-	        if (file.isDirectory()) {
-	            System.out.println("Directory: " + file.getName());
-	            showFiles(file.listFiles()); // Algo class wali recursion Baby
-	        } else {
-	            System.out.println("File: " + file.getName());
-	        }
-	    }
+		for (File file : files) {
+			if (file.isDirectory()) {
+				System.out.println("Directory: " + file.getName());
+				showFiles(file.listFiles()); // Algo class wali recursion Baby
+			} else {
+				System.out.println("File: " + file.getName());
+			}
+		}
 	}
 
 }
