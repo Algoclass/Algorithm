7c74db8d071465274acfdaf6a8ada50ad07331a9
Akshay Mattoo <akshaymattoo@hotmail.com>
2014-11-26 00:32:48 -0600

    Update BoyerMore.java
    updated the boyer more

--- StringMatching/src/algorithms/BoyerMore.java
+++ StringMatching/src/algorithms/BoyerMore.java
@@ -1,25 +1,76 @@
-package algorithms;
+import java.io.*;
+import java.util.*;
 
 public class BoyerMore {
 
-	final static int  NO_OF_CHARS=256;
+	final static int  NO_OF_CHARS=100000;
+	static int testCounter = 0;
+	static int patternCounter =0;
 	
 	public static void main(String args[])
 	{
-	     String t= "AABAACAADAABAAABAA";
-	     char txt[] = t.toCharArray();
-	     String p= "AABA";
-	     char pat[] =p.toCharArray();
-	    search(txt, pat);
+	    /*String t= "In computer science, the Boyer–Moore string search algorithm is an efficient string searching algorithm that is the standard benchmark for practical string search literature.[1] It was developed by Robert S. Boyer and J Strother Moore in 1977.[2] The algorithm preprocesses the string being searched for (the pattern), but not the string being searched in (the text). It is thus well-suited for applications in which the pattern is much shorter than the text or does persist across multiple searches. The Boyer-Moore algorithm uses information gathered during the preprocess step to skip sections of the text, resulting in a lower constant factor than many other string algorithms. In general, the algorithm runs faster as the pattern length increases. The key feature of the algorithm is to match on the tail of the pattern rather than the head, and to skip along the text in jumps of multiple characters rather than searching every single character in the text.";
+	    t= t.replaceAll(" ","");
+	    char txt[] = t.toCharArray();
+	    String p= "It is thus well-suited for applications      in which the pattern is much shorter than the text or does persist across multiple searches.";
+	    p= p.replaceAll(" ","");
+	    char pat[] =p.toCharArray();
+		*/
+		String filepath="by.txt";
+	    ArrayList<String> text = getList(filepath);
+	    ArrayList<String> pattern = getList("ts.txt");
+
+	    for(int i=0;i<pattern.size();i++){
+	    	String t = pattern.get(i);
+	    	t= t.replaceAll(" ","");
+	    	char txt[] = t.toCharArray();
+	    	patternCounter = i; 
+	    	for(int j=0;j<text.size();j++){
+	    		String p = text.get(j);
+	    		p= p.replaceAll(" ","");
+	    		char pat[] =p.toCharArray();
+	    		testCounter = j;
+	    		search(txt, pat);
+	    	}
+	    }
+	    
 	}
 	
+
+	static ArrayList<String> getList(String filepath){
+
+		ArrayList<String> list=null;
+		try{
+
+			File file = new File (filepath);
+			
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			list = new ArrayList<String>();
+			while ((line = br.readLine()) != null) {
+   				// process the line.
+				if(line.length()!=0)
+				{
+					list.add(line);
+				}
+			}
+			 
+		br.close();
+
+		}catch(Exception e){
+
+		}
+		return list;
+	}
+
+
 	/* A pattern searching function that uses Bad Character Heuristic of
 	   Boyer Moore Algorithm */
 	static void search( char txt[],  char pat[])
 	{
 	    int m = pat.length;
 	    int n = txt.length;
-	 
+	 	//System.out.println(n +"---------------"+m);
 	    int badchar[] = new int[NO_OF_CHARS];
 	 
 	    /* Fill the bad character array by calling the preprocessing
@@ -40,7 +91,8 @@ public class BoyerMore {
 	           will become -1 after the above loop */
 	        if (j < 0)
 	        {
-	            System.out.println("pattern occurs at shift"+ s);
+	        	System.out.println("Pattern found for para in pattern"+patternCounter+" for test"+testCounter);
+	            //System.out.println("pattern occurs at shift"+ s);
 	 
 	            /* Shift the pattern so that the next character in text
 	               aligns with the last occurrence of it in pattern.
