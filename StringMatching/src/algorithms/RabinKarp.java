85bbc5eac13696921560f785ba34afe621253053
abhinavbansal246@gmail.com <abhinavbansal246@gmail.com>
2014-12-02 05:02:39 -0600

    changes in rabin karp

--- StringMatching/src/algorithms/RabinKarp.java
+++ StringMatching/src/algorithms/RabinKarp.java
@@ -105,9 +105,6 @@ public class RabinKarp {
 		String patt = String.copyValueOf(pat);
 		String tt = String.copyValueOf(txt);
 
-		if (patt.equals(tt))
-			System.out.println("equals -------");
-
 		int M = pat.length;
 		int N = txt.length;
 		int i, j;
@@ -140,9 +137,9 @@ public class RabinKarp {
 							break;
 						}
 					}
-					System.out.println(match);
+
 					if (match) // if p == t and pat[0...M-1] = txt[i, i+1,
-					// ...i+M-1]
+						// ...i+M-1]
 					{
 						for (int k = 0; k < corpus.size(); k++) {
 							if (corpus.get(k).contains(patt)) {
@@ -193,20 +190,20 @@ public class RabinKarp {
 	/*
 	 * static void search( char txt[],char pat[], int q) { String patt =
 	 * String.copyValueOf(pat);
-	 * 
+	 *
 	 * int M =pat.length; int N = txt.length; int i, j; int p = 0; // hash value
 	 * for pattern int t = 0; // hash value for txt int h = 1;
-	 * 
-	 * 
+	 *
+	 *
 	 * if(M<N){ for (i = 0; i < M-1; i++) h = (h*d)%q;
-	 * 
+	 *
 	 * // Calculate the hash value of pattern and first window of text for (i =
 	 * 0; i < M; i++) { p = (d*p + pat[i])%q; t = (d*t + txt[i])%q;
-	 * 
+	 *
 	 * }
-	 * 
+	 *
 	 * // Slide the pattern over text one by one for (i = 0; i <= N - M; i++) {
-	 * 
+	 *
 	 * // Check the hash values of current window of text and pattern // If the
 	 * hash values match then only check for characters on by one if ( p == t )
 	 * { Check for characters one by one for (j = 0; j < M; j++) { if (txt[i+j]
@@ -219,15 +216,15 @@ public class RabinKarp {
 	 * (sentense_map.get(patt) == 0) { match_count++; int val =
 	 * sentense_map.get(patt); sentense_map.put(patt, val + 1); }
 	 * System.out.println("Pattern found at index"+ i); } }
-	 * 
+	 *
 	 * // Calculate hash value for next window of text: Remove leading digit, //
 	 * add trailing digit if ( i < N-M ) { t = (d*(t - txt[i]*h) + txt[i+M])%q;
-	 * 
+	 *
 	 * // We might get negative value of t, converting it to positive if(t < 0)
 	 * t = (t + q)%q; } }
-	 * 
+	 *
 	 * }
-	 * 
+	 *
 	 * }
 	 */
 
